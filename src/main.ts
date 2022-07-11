4d70926d881fbbb954ec44f38405fe4af9ed3d0a
YeanSe <1730214056@qq.com>
2022-07-11 19:25:58 +0800

    🐛fix the typo

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import App from './App.svelte';
-import tailwindBase from './assets/tailwind.css';
+import tailwindBase from './assets/tailwind.css?inline';
 // 创建顶层 document 环境的 dom 节点
 function createContainerInDocument() {
   const container = document.createElement('div');
-  container.id = 'tempermonkey-app';
+  container.id = 'tampermonkey-app';
   container.style.zIndex = "2147483647";
   container.style.position = "fixed";
   return container;
 }
-// 创建 shadowRoot 环境下的 container 节点（用于挂在 Vue 示例）
+// 创建 shadowRoot 环境下的 container 节点（用于挂载 Svelte 示例）
 function containerInShadowRoot() {
   const container = document.createElement('div');
-  container.id = 'tempermonkey-app-shadow-container';
+  container.id = 'tampermonkey-app-in-shadow-container';
   return container;
 }
 function createDom() {
@@ -23,6 +23,7 @@ function createDom() {
   return containerInShadow;
 }
 function copyCssFromMainDocument(container) {
+  // // 将 main document 中的 css 拷贝到 shadow document 中
   // [...document.querySelectorAll('style')].map((i)=>i.cloneNode(true)).forEach((i)=>container.appendChild(i));
   // [...document.querySelectorAll('link')].map(i=>i.cloneNode(true)).forEach(i=>container.appendChild(i));
   const styleDom = document.createElement('style');
